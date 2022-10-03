63bd68a19ed9463db34d5f577fa6b9a5c298e59e
ivermalme <ivermalme@158-37-229-102.hvl.no>
2022-10-03 12:22:00 +0200

    Matriser O1B

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -4,7 +4,7 @@ public class Matriser {
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
+
 		for (int i = 0; i < matrise.length; i++) {
 
 			for (int j = 0; j < matrise[i].length; j++) {
@@ -17,70 +17,83 @@ public class Matriser {
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-		
+
 		String a = "";
-		for(int i = 0; i< matrise.length; i++) {
-			
-			for(int j = 0; j < matrise.length; j++) {
+		for (int i = 0; i < matrise.length; i++) {
+
+			for (int j = 0; j < matrise.length; j++) {
 				int b = matrise[i][j];
 				a += b + " ";
-				
+
 			}
-			a+= "\n";
+			a += "\n";
 		}
-return a;
-			
-		}
+		return a;
+
+	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		  int[][] nyTab = new int[matrise.length][matrise.length];
 
-	        for (int i = 0; i < matrise.length; i++) {
-	    for (int a = 0; a < matrise.length; a++) {
-	        nyTab[i][a] = (matrise[i][a])*tall;
-	    }
-	
-	}
-	        return nyTab;
+		int[][] nyTab = new int[matrise.length][matrise.length];
+
+		for (int i = 0; i < matrise.length; i++) {
+			for (int a = 0; a < matrise.length; a++) {
+				nyTab[i][a] = (matrise[i][a]) * tall;
+			}
+
+		}
+		return nyTab;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
-		
-		 boolean lik = false; 
-		    int i = 0; 
-		    int c = 0;
-		    while(!lik && i < a.length) {
 
-		        for (i = 0; i < b.length; i++) {
-		            for (c = 0; c < b.length; c++) {
+		boolean lik = false;
+		int i = 0;
+		int c = 0;
+		while (!lik && i < a.length) {
 
-		            if (a[i][c] == b[i][c]) {
-		                lik = true;
+			for (i = 0; i < b.length; i++) {
+				for (c = 0; c < b.length; c++) {
 
-		            }
-		          }
-		        }
-		      }
-		    return lik;
-		    }
-	
-	
+					if (a[i][c] == b[i][c]) {
+						lik = true;
+
+					}
+				}
+			}
+		}
+		return lik;
+	}
+
 	// e)
 	public static int[][] speile(int[][] matrise) {
+		int[][] nytabell = new int[matrise.length][matrise[0].length];
+		for (int i = 0; i < matrise.length; i++) {
+			for (int z = 0; z < matrise[i].length; z++) {
+				nytabell[i][z] = matrise[i][z];
+			}
+		}
+		for (int x = 0; x < matrise.length; x++) {
+			for (int y = 0; y < matrise.length; y++) {
+				nytabell[x][y] = matrise[y][x];
+			}
+		}
+		return nytabell;
 
-		// TODO
-		throw new UnsupportedOperationException("speile ikke implementert");
-	
 	}
 
 	// f)
 	public static int[][] multipliser(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("multipliser ikke implementert");
-	
+		int[][] nytabell = new int[a.length][b[0].length];
+		for (int i = 0; i < nytabell.length; i++) {
+			for (int t = 0; t < nytabell[i].length; t++) {
+				nytabell[i][t] = (a[i][0] * b[0][t]) + (a[i][1] * b[1][t]) + (a[i][2] * b[2][t]);
+			}
+		}
+		return nytabell;
+
 	}
 }
